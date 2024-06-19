f2aaa0041c9bce3a52dfbfad939220cef3cfca88
pank0ff <scorpion-18krek@mail.ru>
2024-06-19 15:43:52 +0300

    init

---
+++ MDF4_SimpleReader.cpp
@@ -0,0 +1,21 @@
+﻿// MDF4_SimpleReader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ImportMdf4.h"
+#include "mdf4.h"
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char* argv[])
+{
+	CMdf4FileImport *pImport = new CMdf4FileImport;
+	//if (pImport->ImportFile("C:\\Temp\\Test.mf4"))
+
+	if (pImport->ImportFile("../Test.mf4"))
+	{
+	}
+	getchar();
+	return 0;
+}
+
